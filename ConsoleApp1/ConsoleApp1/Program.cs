d995e40328e007395d5e55573f782e653ac45c33
kvsStudentGitHub <saieshwary@gmail.com>
2017-12-27 14:57:16 +0000

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //Code added in GitHUb
+            //Code added/editted in GitHUb
         }
     }
 }
