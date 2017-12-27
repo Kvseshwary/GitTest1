84c1f06359e087d4afe6e7223aa9c3a664e04924
saieshwary <saieshwary@gitub.com>
2017-12-27 15:45:33 +0000

    Added Sum

--- ConsoleApp1/ConsoleApp1/Feature1.cs
+++ ConsoleApp1/ConsoleApp1/Feature1.cs
@@ -8,5 +8,13 @@ namespace ConsoleApp1
 {
     class Feature1
     {
+        public int Add()
+        {
+            int x1 = 10;
+            int x2 = 20;
+            int sum;
+            sum = x1 + x2;
+            return sum;
+        }
     }
 }
